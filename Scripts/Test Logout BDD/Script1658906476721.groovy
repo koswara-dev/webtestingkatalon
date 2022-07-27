3536adacbc9d945b22eab589bc1f63b7a2fa293e
Koswara <koswara.c79@gmail.com>
2022-07-28 05:15:39 +0700

    Run 2 Files Feature

--- Scripts/Test Logout BDD/Script1658906476721.groovy
+++ Scripts/Test Logout BDD/Script1658906476721.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-CucumberKW.runFeatureFileWithTags('Include/features/2Login.feature', '@valid')
+CucumberKW.runFeatureFile('Include/features/1Logout.feature')
 
--- Test Suites/Test Suite BDD.ts
+++ Test Suites/Test Suite BDD.ts
@@ -12,6 +12,13 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>c01f001d-0677-4ebd-8024-b5376f0d94bd</testSuiteGuid>
    <testCaseLink>
+      <guid>d88ab43c-97cf-41c1-9206-39ee3f36470a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Login BDD</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
       <guid>3af2b5ff-494c-4fac-bf24-e27736dd24de</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
